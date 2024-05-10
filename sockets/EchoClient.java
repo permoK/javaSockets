9b27ddaabc9238309585670687657447bf3e1e7d
MugumoPerm <perminusk21@gmail.com>
2024-05-11 01:51:37 +0300

    update the server communication back to back

--- sockets/EchoClient.java
+++ sockets/EchoClient.java
@@ -1,17 +1,29 @@
 import java.net.Socket;
+import java.io.*;
 
 public class EchoClient {
-	public static void main(String[] args) {
-		try 
-		{
-			System.out.println("Client started");
-			Socket soc = new Socket("localhost",1212);
+    public static void main(String[] args) {
+        try {
+            System.out.println("Client started");
+            // Socket soc = new Socket("54.237.50.146",3306);
+            Socket soc = new Socket("localhost", 1212);
 
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
 
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+            // Loop to continuously send and receive messages
+            while (true) {
+                System.out.println("Enter message to send to server");
+                String str = userInput.readLine();
+                out.println(str);
 
-	}
+                String sstr = in.readLine();
+                System.out.println("Message from server: " + sstr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
--- sockets/EchoServer.java
+++ sockets/EchoServer.java
@@ -1,19 +1,56 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 
 public class EchoServer{
-	public static void main(String[] args) {
-		try
-		{
-			System.out.println("Server started");
-			ServerSocket ss = new ServerSocket(1212);
+    public static void main(String[] args) {
+        try {
+            System.out.println("Waiting for clients to connect...");
+            ServerSocket ss = new ServerSocket(1212);
+            
+            // Keep the server running indefinitely
+            while (true) {
+                Socket soc = ss.accept();
+                System.out.println("Connection established with " + soc.getInetAddress());
 
-			Socket soc = ss.accept();
-			
-			System.out.println("Connection established");
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                // Handle the client request in a separate thread to allow concurrent connections
+                new Thread(new ClientHandler(soc)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // receive a message from the client
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String str = in.readLine();
+            System.out.println("Message from client: " + str);
+
+            // Enter the response to send to client
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter response: ");
+            String response = userInput.readLine();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(response);
+
+            // Close the resources
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
