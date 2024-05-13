77f670736484cb7c2a0c2244d0e1726011d0c362
MugumoPerm <perminusk21@gmail.com>
2024-05-13 19:47:54 +0300

    update

---
+++ UDP/EchoClient.java
@@ -0,0 +1,33 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.*;
+
+public class EchoClient {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+            
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Send: ");
+            String str = userInput.readLine();
+            sendData = str.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 1212);
+            socket.send(sendPacket);
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(receivePacket);
+            String receivedStr = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Received: " + receivedStr);
+
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ UDP/EchoServer.java
@@ -0,0 +1,34 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.io.*;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(1212);
+            byte[] receiveData = new byte[1024];
+            
+            System.out.println("Server started...");
+            
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                socket.receive(receivePacket);
+                String receivedStr = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Client: " + receivedStr);
+                
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Enter response: ");
+                String response = userInput.readLine();
+                byte[] sendData = response.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+                socket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
